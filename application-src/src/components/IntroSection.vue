b4c3464bf895c7dc5a478b5d07d411a10da4d41a
Silas Jimmy <jimmysilas17@gmail.com>
2025-10-12 20:56:09 +0300

    refactor: update intro component

--- application-src/src/components/IntroSection.vue
+++ application-src/src/components/IntroSection.vue
@@ -1,7 +1,40 @@
 <template>
-    <h1>Intro</h1>
+  <div class="intro-wrapper">
+    <Avatar
+      image="https://firebasestorage.googleapis.com/v0/b/silasjimmydev.firebasestorage.app/o/images%2Favatars%2Fsilas-jimmy.jpg?alt=media&token=6b01579c-42ce-4520-b8bb-132787b86a65"
+      size="xlarge"
+      shape="circle"
+      alt="silas jimmy"
+    />
+
+    <h1>
+      Hello, i am
+      <br />
+      <strong>Silas Jimmy</strong>
+    </h1>
+
+    <h2>Software Engineer</h2>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel dolor pariatur est. Doloribus,
+      et placeat unde id ullam neque exercitationem, nulla fugit explicabo blanditiis eaque
+      accusamus voluptatibus odit esse porro.
+    </p>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Avatar } from 'primevue'
+</script>
 
-<style scoped></style>
+<style scoped>
+.intro-wrapper {
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  text-align: center;
+  margin: 0 auto;
+}
+</style>
--- application-src/src/components/ProjectSection.vue
+++ application-src/src/components/ProjectSection.vue
@@ -1,7 +1,19 @@
 <template>
-    <h1>Projects</h1>
+  <Carousel circular>
+    <Image src='https://primefaces.org/cdn/primevue/images/product/' alt="image" width="250" />
+    <Image src='https://primefaces.org/cdn/primevue/images/product/' alt="image" width="250" />
+    <Image src='https://primefaces.org/cdn/primevue/images/product/' alt="image" width="250" />
+    <Image src='https://primefaces.org/cdn/primevue/images/product/' alt="image" width="250" />
+    <Image src='https://primefaces.org/cdn/primevue/images/product/' alt="image" width="250" />
+  </Carousel>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Carousel from 'primevue/carousel'
+import Image from 'primevue/image';
+import { ref } from 'vue';
+
+const carouselOptions = ref([])
+</script>
 
 <style scoped></style>
