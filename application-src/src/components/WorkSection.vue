0fe9ca6eb78df451b6e2ed3940645f0a64510f9a
Silas Jimmy <jimmysilas17@gmail.com>
2025-10-13 09:09:47 +0300

    refactor: update work section styles

--- application-src/src/components/WorkSection.vue
+++ application-src/src/components/WorkSection.vue
@@ -1,7 +1,59 @@
 <template>
-    <h1>Work experience</h1>
+  <div class="border">
+    <h1 class="text-2xl font-medium mb-2">Work Expereince</h1>
+
+    <div>
+      <Timeline :value="experienceData" align="alternate">
+        <template #content="slotProps">
+          <h1 class="font-bold mb-1">{{ slotProps.item.title }}</h1>
+
+          <h2 class="text-sm font-light">
+            {{ slotProps.item.company }}
+            <span class="mx-0.5">-</span>
+            {{ slotProps.item.type }}
+          </h2>
+
+          <span class="text-xs mb-2">
+            {{ shortDateFormat(slotProps.item.time[0]) }}
+            <span class="mx-0.5">-</span>
+            {{ new Date(slotProps.item.time[1]) === new Date() ? 'Present' : shortDateFormat(slotProps.item.time[1]) }}
+          </span>
+
+          <p class="text-sm text-gray-300 font-light">{{ slotProps.item.description }}</p>
+        </template>
+      </Timeline>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Timeline from 'primevue/timeline'
+import { shortDateFormat } from '@/utils/datetimeUtils'
+import { ref } from 'vue'
+
+const experienceData = ref([
+    {
+    time: [new Date(), new Date()],
+    type: 'Full-time',
+    title: 'Software Engineer',
+    company: 'Freelance',
+    description: 'something about the job',
+  },
+  {
+    time: [new Date(), new Date()],
+    type: 'Part-time',
+    title: 'Software Engineer',
+    company: 'Dema Africa',
+    description: 'something about the job',
+  },
+  {
+    time: [new Date(), new Date()],
+    type: 'Contract',
+    title: 'Fullstack Software Developer',
+    company: 'Dema Africa',
+    description: 'something about the job',
+  },
+])
+</script>
 
 <style scoped></style>
---
+++ application-src/src/utils/datetimeUtils.ts
@@ -0,0 +1,3 @@
+export const shortDateFormat = (date: Date) => {
+    return date.getDate()
+}
