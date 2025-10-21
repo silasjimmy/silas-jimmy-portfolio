43687e634659418ac3896737810e1f9e2b569fe6
Silas Jimmy <jimmysilas17@gmail.com>
2025-10-21 11:10:51 +0300

    config: setup primevue with tailwindcss

--- application-src/src/main.ts
+++ application-src/src/main.ts
@@ -6,9 +6,23 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      cssLayer: {
+        name: 'primevue',
+        order: 'theme, base, primevue',
+      },
+    },
+  },
+})
 
 app.mount('#app')
