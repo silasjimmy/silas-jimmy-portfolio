6378e73c6cb4698ae4d3bdb16a9454155ae68d4b
Silas Jimmy <jimmysilas17@gmail.com>
2025-12-05 21:26:19 +0300

    refactor: update navbar layout

--- application-src/src/components/NavbarSection.vue
+++ application-src/src/components/NavbarSection.vue
@@ -1,7 +1,30 @@
 <template>
-  <nav class="fixed left-[50%] translate-x-[-50%]">Navbar</nav>
+  <nav
+    class="bg-gray-200 shadow-md fixed top-5 left-[50%] translate-x-[-50%] rounded-[36px] px-[26px] py-2 flex items-center gap-6"
+  >
+    <a class="text-sm" href="#">Home</a>
+    <a class="text-sm" href="#">About Me</a>
+    <a class="text-sm" href="#">Projects</a>
+    <a class="text-sm" href="#">Volunteer</a>
+    <a class="text-sm" href="#">Blog</a>
+    <Button rounded icon="pi pi-sun" variant="text" size="small" aria-label="App theme" />
+  </nav>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
 
-<style scoped></style>
+<style scoped>
+.p-button-sm {
+  padding: 8px 8px;
+}
+
+.p-button-sm.p-button-icon-only.p-button-rounded {
+  height: 24px;
+}
+
+.p-button-sm.p-button-icon-only {
+  width: 24px;
+}
+</style>
