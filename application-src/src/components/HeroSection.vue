0339bd31f075484115bda4e4da0159196ed8901d
Silas Jimmy <jimmysilas17@gmail.com>
2025-12-05 22:04:24 +0300

    refactor: update hero section layout

--- application-src/src/components/HeroSection.vue
+++ application-src/src/components/HeroSection.vue
@@ -1,19 +1,37 @@
 <template>
-  <div class="hero-section">
+  <div class="pt-[100px]">
     <div class="hero-text flex flex-col gap-4 items-center pt-[100px] mb-[50px]">
-      <Avatar size="xlarge" shape="circle" />
-
-      <div class="w-3/4 flex flex-col gap-2 items-center">
-        <div class="title-wrapper w-1/2">
-          <div class="bg-gray-100 h-14">title</div>
-        </div>
-
-        <div class="bg-gray-100 h-10">subtitle</div>
+      <div class="border rounded-full flex items-center justify-center p-0.5">
+        <Avatar size="xlarge" shape="circle" />
       </div>
 
-      <div class="bg-gray-100 h-4">small text</div>
+      <div class="w-3/4 flex flex-col gap-4 items-center">
+        <div class="title-wrapper w-4/5 text-center">
+          <h1 class="text-[42px] leading-none mb-2.5 font-bold">Hello, I'm Silas Jimmy</h1>
 
-      <div class="bg-gray-100 h-6">icons</div>
+          <h1 class="text-[42px] leading-none font-bold">Data Scientist</h1>
+        </div>
+
+        <p class="text-base leading-6 text-center">
+          I help organizations design scalable, user-centered products, implement automated
+          architectures powered by cloud technologies, and drive data-informed decisions through
+          machine learning and AI.
+        </p>
+      </div>
+
+      <div class="flex items-center gap-3">
+        <span class="w-2 h-2 border rounded-full"></span>
+
+        <small>Available for new projects</small>
+      </div>
+
+      <div class="flex items-center gap-6">
+        <span class="pi pi-linkedin" style="font-size: 18px"></span>
+
+        <span class="pi pi-twitter" style="font-size: 18px"></span>
+
+        <span class="pi pi-github" style="font-size: 18px"></span>
+      </div>
     </div>
 
     <div class="hero-carousel">
@@ -26,4 +44,9 @@
 import Avatar from 'primevue/avatar'
 </script>
 
-<style scoped></style>
+<style scoped>
+.p-avatar-xl {
+  width: 70px;
+  height: 70px;
+}
+</style>
--- application-src/src/components/NavbarSection.vue
+++ application-src/src/components/NavbarSection.vue
@@ -1,30 +1,16 @@
 <template>
   <nav
-    class="bg-gray-200 shadow-md fixed top-5 left-[50%] translate-x-[-50%] rounded-[36px] px-[26px] py-2 flex items-center gap-6"
+    class="bg-gray-200 shadow-md fixed top-5 left-[50%] translate-x-[-50%] rounded-[36px] px-[26px] py-4 flex items-center gap-6"
   >
     <a class="text-sm" href="#">Home</a>
     <a class="text-sm" href="#">About Me</a>
     <a class="text-sm" href="#">Projects</a>
     <a class="text-sm" href="#">Volunteer</a>
     <a class="text-sm" href="#">Blog</a>
-    <Button rounded icon="pi pi-sun" variant="text" size="small" aria-label="App theme" />
+    <span class="pi pi-sun" style="font-size: 16px"></span>
   </nav>
 </template>
 
-<script setup lang="ts">
-import Button from 'primevue/button'
-</script>
+<script setup lang="ts"></script>
 
-<style scoped>
-.p-button-sm {
-  padding: 8px 8px;
-}
-
-.p-button-sm.p-button-icon-only.p-button-rounded {
-  height: 24px;
-}
-
-.p-button-sm.p-button-icon-only {
-  width: 24px;
-}
-</style>
+<style scoped></style>
