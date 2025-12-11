117ace42b947bcb5ede6b812f885b0da0f9ca677
Silas Jimmy <jimmysilas17@gmail.com>
2025-12-11 15:03:23 +0300

    refactor: update about me view layout

--- application-src/src/views/AboutMeView.vue
+++ application-src/src/views/AboutMeView.vue
@@ -1,7 +1,106 @@
 <template>
-  <div class="border">
-    <h1>About Me</h1>
-  </div>
+  <main class="lg:border-x 2xl:w-1/2 mx-auto">
+    <navbar-section></navbar-section>
+
+    <div class="pt-[200px] px-[30px]">
+      <div class="flex items-center gap-[50px] mb-[100px]">
+        <div class="flex flex-col gap-4">
+          <h1 class="text-4xl leading-none font-bold">About Me</h1>
+
+          <p class="text-sm leading-[22px] text-left">
+            I am a problem solver who thrives in environments where curiosity and creativity
+            intersect. Whether developing software, designing architectures, or shaping user
+            experiences, I am driven to push beyond boundaries and deliver meaningful impact.
+          </p>
+        </div>
+
+        <div>
+          <div class="w-[150px] h-[150px] rounded-lg ring-2 ring-gray-100 rotate-4 p-0.5">
+            <div class="w-full h-full bg-gray-100 rounded-lg"></div>
+          </div>
+        </div>
+      </div>
+
+      <div class="mb-[30px]">
+        <p class="text-sm leading-[22px] text-left">
+          Lorem ipsum dolor sit amet consectetur. Arcu leo eu pretium duis lacus sed eget hendrerit.
+          Amet lectus tortor maecenas ut ipsum rutrum tristique augue. Augue ac iaculis nec a
+          scelerisque viverra. Molestie lectus facilisis a sapien pellentesque odio eget lacus.
+          Praesent hendrerit a sed ultrices accumsan consequat amet hendrerit eu. Vitae lectus risus
+          gravida aliquet at feugiat. Vestibulum interdum faucibus faucibus ante. Nam massa
+          venenatis nunc leo urna amet. Ornare a turpis lobortis pellentesque. Diam ornare nunc
+          aliquam ultricies lacus facilisis. Mattis auctor potenti sit eu vitae vestibulum ipsum
+          nunc. Quam neque tincidunt odio id at pellentesque feugiat eu placerat. Dolor tincidunt
+          morbi proin sit. Nullam velit adipiscing tristique duis amet mauris tellus. Urna non
+          vulputate senectus volutpat viverra neque. Aliquet eu lacus pharetra nunc. Aliquam sed dui
+          adipiscing nec. Arcu sed porttitor arcu vitae lobortis et nunc eget. Porttitor tincidunt
+          diam augue quis viverra ullamcorper. Id rhoncus ut convallis nulla amet id eget blandit.
+        </p>
+      </div>
+
+      <div class="mb-[30px]">
+        <h3 class="mb-4 text-[20px] leading-none font-semibold">Skills</h3>
+
+        <p class="text-sm leading-[22px] text-left">
+          Lorem ipsum dolor sit amet consectetur. Arcu leo eu pretium duis lacus sed eget hendrerit.
+          Amet lectus tortor maecenas ut ipsum rutrum tristique augue. Augue ac iaculis nec a
+          scelerisque viverra. Molestie lectus facilisis a sapien pellentesque odio eget lacus.
+          Praesent hendrerit a sed ultrices accumsan consequat amet hendrerit eu. Vitae lectus risus
+          gravida aliquet at feugiat. Vestibulum interdum faucibus faucibus ante. Nam massa
+          venenatis nunc leo urna amet. Ornare a turpis lobortis pellentesque. Diam ornare nunc
+          aliquam ultricies lacus facilisis. Mattis auctor potenti sit eu vitae vestibulum ipsum
+          nunc. Quam neque tincidunt odio id at pellentesque feugiat eu placerat. Dolor tincidunt
+          morbi proin sit. Nullam velit adipiscing tristique duis amet mauris tellus. Urna non
+          vulputate senectus volutpat viverra neque. Aliquet eu lacus pharetra nunc. Aliquam sed dui
+          adipiscing nec. Arcu sed porttitor arcu vitae lobortis et nunc eget. Porttitor tincidunt
+          diam augue quis viverra ullamcorper. Id rhoncus ut convallis nulla amet id eget blandit.
+        </p>
+      </div>
+
+      <div class="mb-[30px]">
+        <h3 class="mb-4 text-[20px] leading-none font-semibold">Motivation</h3>
+
+        <p class="text-sm leading-[22px] text-left">
+          Lorem ipsum dolor sit amet consectetur. Arcu leo eu pretium duis lacus sed eget hendrerit.
+          Amet lectus tortor maecenas ut ipsum rutrum tristique augue. Augue ac iaculis nec a
+          scelerisque viverra. Molestie lectus facilisis a sapien pellentesque odio eget lacus.
+          Praesent hendrerit a sed ultrices accumsan consequat amet hendrerit eu. Vitae lectus risus
+          gravida aliquet at feugiat. Vestibulum interdum faucibus faucibus ante. Nam massa
+          venenatis nunc leo urna amet. Ornare a turpis lobortis pellentesque. Diam ornare nunc
+          aliquam ultricies lacus facilisis. Mattis auctor potenti sit eu vitae vestibulum ipsum
+          nunc. Quam neque tincidunt odio id at pellentesque feugiat eu placerat. Dolor tincidunt
+          morbi proin sit. Nullam velit adipiscing tristique duis amet mauris tellus. Urna non
+          vulputate senectus volutpat viverra neque. Aliquet eu lacus pharetra nunc. Aliquam sed dui
+          adipiscing nec. Arcu sed porttitor arcu vitae lobortis et nunc eget. Porttitor tincidunt
+          diam augue quis viverra ullamcorper. Id rhoncus ut convallis nulla amet id eget blandit.
+        </p>
+      </div>
+
+      <div class="mb-[30px]">
+        <h3 class="mb-4 text-[20px] leading-none font-semibold">Hobbies</h3>
+
+        <p class="text-sm leading-[22px] text-left">
+          Lorem ipsum dolor sit amet consectetur. Arcu leo eu pretium duis lacus sed eget hendrerit.
+          Amet lectus tortor maecenas ut ipsum rutrum tristique augue. Augue ac iaculis nec a
+          scelerisque viverra. Molestie lectus facilisis a sapien pellentesque odio eget lacus.
+          Praesent hendrerit a sed ultrices accumsan consequat amet hendrerit eu. Vitae lectus risus
+          gravida aliquet at feugiat. Vestibulum interdum faucibus faucibus ante. Nam massa
+          venenatis nunc leo urna amet. Ornare a turpis lobortis pellentesque. Diam ornare nunc
+          aliquam ultricies lacus facilisis. Mattis auctor potenti sit eu vitae vestibulum ipsum
+          nunc. Quam neque tincidunt odio id at pellentesque feugiat eu placerat. Dolor tincidunt
+          morbi proin sit. Nullam velit adipiscing tristique duis amet mauris tellus. Urna non
+          vulputate senectus volutpat viverra neque. Aliquet eu lacus pharetra nunc. Aliquam sed dui
+          adipiscing nec. Arcu sed porttitor arcu vitae lobortis et nunc eget. Porttitor tincidunt
+          diam augue quis viverra ullamcorper. Id rhoncus ut convallis nulla amet id eget blandit.
+        </p>
+      </div>
+
+      <footer-section></footer-section>
+    </div>
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NavbarSection from '@/components/NavbarSection.vue'
+import FooterSection from '@/components/FooterSection.vue'
+</script>
