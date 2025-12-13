e23c0a4e79478e9b99f9f5a38fbdee1d29ac7239
Silas Jimmy <jimmysilas17@gmail.com>
2025-12-13 15:29:05 +0300

    refactor: update volunteer work view layout

--- application-src/src/views/VolunteerView.vue
+++ application-src/src/views/VolunteerView.vue
@@ -1,41 +1,48 @@
 <template>
-  <main class="2xl:w-1/2 xl:w-2/3 lg:w-3/4 lg:border-x mx-auto">
-    <navbar-section></navbar-section>
+  <navbar-section></navbar-section>
 
-    <div class="pt-[200px] px-[30px]">
-      <div class="flex items-center gap-[50px] mb-[100px]">
+  <main class="2xl:w-1/2 xl:w-2/3 lg:w-3/4 lg:border-x mx-auto">
+    <div class="pt-[100px] md:pt-[150px] px-[30px]">
+      <div
+        class="flex flex-col md:flex-row items-center gap-[30px] md:gap-[50px] mb-[50px] md:mb-[50px]"
+      >
         <div class="flex flex-col gap-4">
-          <h1 class="text-4xl leading-none font-bold">
-            Volunter Work
-          </h1>
+          <h1 class="text-3xl md:text-4xl leading-none font-bold">Volunteer Work</h1>
 
           <p class="text-sm leading-[22px] text-left">
-            Lorem ipsum dolor sit amet consectetur. Vitae tempor quam pretium facilisis volutpat
-            consequat pharetra egestas imperdiet. Penatibus nullam vulputate id dolor purus amet.
-            Nunc faucibus pellentesque bibendum mattis eu. Rhoncus dui sit nulla pharetra. Nisi
-            habitasse.
+            It is said that with great power comes great responsibility. I have volunteered to be a
+            leader in various capacities just to get the opportunity to teach, mentor and guide
+            others in the tech industry. Below are a few roles that i have held as a volunteer in my
+            line of work.
           </p>
-
-          <div class="flex gap-6">
-            <Button label="Let's talk" size="small"></Button>
-
-            <Button label="Send an email" size="small"></Button>
-          </div>
         </div>
 
         <div>
-          <div class="w-[150px] h-[150px] rounded-lg ring-2 ring-gray-100 rotate-4 p-0.5">
-            <div class="w-full h-full bg-gray-100 rounded-lg"></div>
+          <div class="w-[150px] h-[200px] rounded-lg ring-2 ring-gray-200 md:rotate-4 p-0.5">
+            <Image :pt="volunteerImagePt" src="images/silas-jimmy-talk.jpeg" alt="Silas Jimmy" />
           </div>
         </div>
       </div>
-
-      <footer-section></footer-section>
     </div>
+
+    <footer-section></footer-section>
   </main>
 </template>
 
 <script setup lang="ts">
 import NavbarSection from '@/components/NavbarSection.vue'
 import FooterSection from '@/components/FooterSection.vue'
+import Image from 'primevue/image'
+
+// Define image pass-through properties for the about image
+const volunteerImagePt = {
+  image: {
+    class: 'rounded-lg',
+    style: {
+      height: '100%',
+      width: '100%',
+      'object-fit': 'cover',
+    },
+  },
+}
 </script>
