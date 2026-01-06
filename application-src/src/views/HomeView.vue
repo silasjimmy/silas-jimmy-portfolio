b8606f7d4687b5cc2c7b0af40f9be9a9dee63737
Silas Jimmy <jimmysilas17@gmail.com>
2026-01-06 12:31:49 +0300

    refactor: create faqs section component

--- application-src/src/views/HomeView.vue
+++ application-src/src/views/HomeView.vue
@@ -9,6 +9,8 @@
     <latest-articles-section></latest-articles-section>
 
     <testimonials-section></testimonials-section>
+
+    <faqs-section></faqs-section>
   </div>
 </template>
 
@@ -18,5 +20,5 @@ import AboutMeSection from '@/components/sections/AboutMeSection.vue'
 import WorkExperienceSection from '@/components/sections/WorkExperienceSection.vue'
 import LatestArticlesSection from '@/components/sections/LatestArticlesSection.vue'
 import TestimonialsSection from '@/components/sections/TestimonialsSection.vue'
-import GetInTouchSection from '@/components/GetInTouchSection.vue'
+import FaqsSection from '@/components/sections/FaqsSection.vue'
 </script>
