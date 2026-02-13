cf5ce2bd2d2e602ed56d4ad4d4d409166284114a
Silas Jimmy <jimmysilas17@gmail.com>
2026-02-13 09:04:22 +0300

    refactor: update latest article card styles

--- application-src/src/components/cards/LatestArticleCard.vue
+++ application-src/src/components/cards/LatestArticleCard.vue
@@ -1,6 +1,6 @@
 <template>
   <article
-    class="flex flex-col overflow-hidden lg:grid-cols-2 gap-x-8 transition relative lg:items-start lg:flex ring-0 hover:ring-0"
+    class="border-b border-zinc-200 dark:border-zinc-500 flex flex-col overflow-hidden pb-4 lg:grid-cols-2 gap-x-8 transition relative lg:items-start lg:flex ring-0 hover:ring-0"
   >
     <router-link :to="`/blog/${props.id}`" class="py-4 sm:py-6">
       <div class="mb-2">
@@ -16,7 +16,7 @@
       </p>
     </router-link>
 
-    <router-link :to="`/blog/${props.id}`"  class="text-xs text-green-500">Read Article</router-link>
+    <router-link :to="`/blog/${props.id}`"  class="text-xs text-green-500 dark:text-green-400 hover:text-green-400 dark:hover:text-green-500 transition-colors">Read Article</router-link>
   </article>
 </template>
 
